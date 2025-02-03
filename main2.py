2a4dfb0c0487d9efe5e05cc37a8119cc939b79f6
anant-c <canant448@gmail.com>
2025-02-03 19:08:14 +0530

    final-render-backend

--- main2.py
+++ main2.py
@@ -49,4 +49,4 @@ def get_file(filename):
         return jsonify({"error": "File not found"}), 404
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
